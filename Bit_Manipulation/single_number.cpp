35f739b2b0cad528b0c53f569630cbd4a8fdf756
kuluru vineeth kumar reddy <47528651+kuluruvineeth@users.noreply.github.com>
2020-11-28 11:04:56 +0530

    code to find single integer in array

---
+++ Bit_Manipulation/single_number.cpp
@@ -0,0 +1,31 @@
+//Given an array of integers, every element appears twice except for one. Find that single one.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int single(vector<int> &v)
+{
+  if (v.size() == 0)
+  {
+    return 0;
+  }
+  int first = v[0];
+  for (int i = 1; i < v.size(); i++)
+  {
+    first = first ^ v[i];
+  }
+  return first;
+}
+
+int main()
+{
+  vector<int> v(5);
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  v.push_back(3);
+  v.push_back(1);
+  int alone = single(v);
+  cout << "single integer in array is : " << alone << endl;
+  return 0;
+}
